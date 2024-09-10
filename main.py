39ef196342f1553c7cc4ee5f32b4ee668418918f
CrJunior08 <cleristoncontato@outlook.com>
2024-09-10 18:04:16 -0300

    adicionado tratamento de variaveis

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+
+if not account_sid or not auth_token:
+    raise ValueError("As variáveis de ambiente TWILIO_ACCOUNT_SID e TWILIO_AUTH_TOKEN devem ser definidas.")
+
+client = Client(account_sid, auth_token)
+
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        message = client.messages.create(
+            to="+5511949401482",
+            from_="+12513136551",
+            body=f'No mês {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        print(message.sid)
